b3b8025327f43813f3d3032905efed7d92541f0f
figueiredosoftware <figueiredosoftware@gmail.com>
2021-06-29 16:01:56 -0300

    Add project files.

---
+++ ClienteServicosWebAPI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; //obrigatorio
+using System.Net.Http; //obrigatorio
+using System.Net.Http.Headers; //obrigatorio
+
+
+namespace ClienteServicosWebAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("teste");
+            Console.ReadLine();
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            var formato = new MediaTypeWithQualityHeaderValue("application/json");
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44344/");
+                client.DefaultRequestHeaders.Accept.Clear(); //limpa todos padrões que tiverem setados por default
+                client.DefaultRequestHeaders.Accept.Add(formato); //seta informa indica que o novo formato é esse no caso json
+                var resposta = await client.GetAsync("api/Produto"); //await força esperar porque é assincrono
+                string conteudo = await resposta.Content.ReadAsAsync<string>(); //se retorna string então aqui passa string, se retorna array de produto então passa array de produto depende do que a controller retorra tem que por igual aqui
+                Console.WriteLine(conteudo);
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ ExemploWebApi/Controllers/ProdutoController.cs
@@ -0,0 +1,41 @@
+﻿using ExemploWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExemploWebApi.Controllers
+{
+    public class ProdutoController : ApiController
+    {
+        // GET api/<controller>
+        public string Get()
+        {
+            return "Isto vem da web API";
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public string Post([FromBody] Produto produto)
+        {
+            return "Você enviou :" + produto.Nome + " " + produto.Preco.ToString("C");
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
